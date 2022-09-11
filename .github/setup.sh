4f624bd13b994b9299967580781aed25d029ccab
Avijit Dey <subhoavijit@gmail.com>
2022-09-11 08:13:18 +0000

    Set up CI Pipeline

---
+++ .github/setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+apt-get update -qq
+
+# install boost
+BOOST_MAJOR_VERSION="1"
+BOOST_MINOR_VERSION="79"
+
+wget "https://boostorg.jfrog.io/artifactory/main/release/"${BOOST_MAJOR_VERSION}"."${BOOST_MINOR_VERSION}".0/source/boost_"${BOOST_MAJOR_VERSION}"_"${BOOST_MINOR_VERSION}"_0.tar.gz"
+
+mkdir boost
+tar -zxvf boost_${BOOST_MAJOR_VERSION}_${BOOST_MINOR_VERSION}_0.tar.gz -C boost
+cd boost/boost_${BOOST_MAJOR_VERSION}_${BOOST_MINOR_VERSION}_0
+./bootstrap.sh
+./b2 variant=release install
+cd ../../
+
+# install dlt daemon
+DLT_MAJOR_VERSION="2"
+DLT_MINOR_VERSION="18"
+DLT_PATCH_VERSION="8"
+wget "https://github.com/COVESA/dlt-daemon/archive/refs/tags/v2.18.8.tar.gz"
+mkdir dlt-daemon 
+tar -xvf v${DLT_MAJOR_VERSION}.${DLT_MINOR_VERSION}.${DLT_PATCH_VERSION}.tar.gz -C dlt-daemon
+mkdir -p dlt-daemon/dlt-daemon-${DLT_MAJOR_VERSION}.${DLT_MINOR_VERSION}.${DLT_PATCH_VERSION}/build
+cd dlt-daemon/dlt-daemon-${DLT_MAJOR_VERSION}.${DLT_MINOR_VERSION}.${DLT_PATCH_VERSION}
+cmake -B build -DCMAKE_BUILD_TYPE=Release
+cmake --build build --config Release
+cmake --install build --config Release
