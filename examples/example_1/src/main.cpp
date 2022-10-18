1aa043b77b2bc4c33d3d5486faa1f9724de9e462
Avijit Dey <subhoavijit@gmail.com>
2022-10-18 20:53:26 +0200

    Update example code with new changes

--- examples/example_1/src/main.cpp
+++ examples/example_1/src/main.cpp
@@ -16,19 +16,19 @@ int main(void) {
     // Initialize the Diagnostic Client library
     diagclient->Initialize();
     
-    // Get conversion for tester one
-    diag::client::conversion::DiagClientConversion& diag_client_conversion1 =
-                diagclient->GetDiagnosticClientConversion("DiagTesterOne");
+    // Get conversation for tester one
+    diag::client::conversation::DiagClientConversation& diag_client_conversation1 =
+                diagclient->GetDiagnosticClientConversation("DiagTesterOne");
     
-    // Start the conversion for tester one
-    diag_client_conversion1.Startup();
+    // Start the conversation for tester one
+    diag_client_conversation1.Startup();
 
-    // Get conversion for tester two
-    diag::client::conversion::DiagClientConversion& diag_client_conversion2 =
-                diagclient->GetDiagnosticClientConversion("DiagTesterTwo");
+    // Get conversation for tester two
+    diag::client::conversation::DiagClientConversation& diag_client_conversation2 =
+                diagclient->GetDiagnosticClientConversation("DiagTesterTwo");
     
-    // Start the conversion for tester two
-    diag_client_conversion2.Startup();
+    // Start the conversation for tester two
+    diag_client_conversation2.Startup();
 
     // Create a uds payload 10 01 ( Default Session )
     diag::client::uds_message::ByteVector payload_1;
@@ -43,62 +43,62 @@ int main(void) {
     {
         // Create a uds message with payload for ECU1
         diag::client::uds_message::UdsRequestMessagePtr uds_message_1 = 
-                            std::make_unique<UdsMessage>("192.168.0.1", payload_1);
+                            std::make_unique<UdsMessage>("192.168.0.2", payload_1);
 
         // Create a uds message with payload for ECU2
         diag::client::uds_message::UdsRequestMessagePtr uds_message_2 = 
-                            std::make_unique<UdsMessage>("192.168.0.2", payload_2);
+                            std::make_unique<UdsMessage>("192.168.0.3", payload_2);
         
-        // Connect Tester One to ECU1 with remote ip address 192.168.0.1
-        diag_client_conversion1.ConnectToDiagServer(uds_message_1->GetHostIpAddress());
+        // Connect Tester One to ECU1 with remote ip address 192.168.0.2
+        diag_client_conversation1.ConnectToDiagServer(uds_message_1->GetHostIpAddress());
         
-        // Connect Tester Two to ECU2 with remote ip address 192.168.0.2
-        diag_client_conversion2.ConnectToDiagServer(uds_message_2->GetHostIpAddress());
+        // Connect Tester Two to ECU2 with remote ip address 192.168.0.3
+        diag_client_conversation2.ConnectToDiagServer(uds_message_2->GetHostIpAddress());
         
         // Use Tester One to send the diagnostic message to ECU1
-        std::pair<diag::client::conversion::DiagClientConversion::DiagResult, diag::client::uds_message::UdsResponseMessagePtr> 
-            ret_val_1{diag::client::conversion::DiagClientConversion::DiagResult::kDiagFailed, nullptr};
+        std::pair<diag::client::conversation::DiagClientConversation::DiagResult, diag::client::uds_message::UdsResponseMessagePtr> 
+            ret_val_1{diag::client::conversation::DiagClientConversation::DiagResult::kDiagFailed, nullptr};
         
-        ret_val_1 = diag_client_conversion1.SendDiagnosticRequest(std::move(uds_message_1));
+        ret_val_1 = diag_client_conversation1.SendDiagnosticRequest(std::move(uds_message_1));
 
         if(ret_val_1.first == 
-            diag::client::conversion::DiagClientConversion::DiagResult::kDiagSuccess) {
-            std::cout << "diag_client_conversion1 Total size: " << ret_val_1.second->GetPayload().size() << std::endl;
+            diag::client::conversation::DiagClientConversation::DiagResult::kDiagSuccess) {
+            std::cout << "diag_client_conversation1 Total size: " << ret_val_1.second->GetPayload().size() << std::endl;
             
             // Print the payload
             for (auto const byte: ret_val_1.second->GetPayload()) {
-                std::cout << "diag_client_conversion1 byte: " << std::hex << static_cast<int>(byte) << std::endl;
+                std::cout << "diag_client_conversation1 byte: " << std::hex << static_cast<int>(byte) << std::endl;
             }
         }
 
         // Use Tester Two to send the diagnostic message to ECU2
-        std::pair<diag::client::conversion::DiagClientConversion::DiagResult, diag::client::uds_message::UdsResponseMessagePtr> 
-            ret_val_2{diag::client::conversion::DiagClientConversion::DiagResult::kDiagFailed, nullptr};
+        std::pair<diag::client::conversation::DiagClientConversation::DiagResult, diag::client::uds_message::UdsResponseMessagePtr> 
+            ret_val_2{diag::client::conversation::DiagClientConversation::DiagResult::kDiagFailed, nullptr};
         
-        ret_val_2 = diag_client_conversion2.SendDiagnosticRequest(std::move(uds_message_2));
+        ret_val_2 = diag_client_conversation2.SendDiagnosticRequest(std::move(uds_message_2));
 
         if(ret_val_2.first == 
-            diag::client::conversion::DiagClientConversion::DiagResult::kDiagSuccess) {
-            std::cout << "diag_client_conversion2 Total size: " << ret_val_2.second->GetPayload().size() << std::endl;
+            diag::client::conversation::DiagClientConversation::DiagResult::kDiagSuccess) {
+            std::cout << "diag_client_conversation2 Total size: " << ret_val_2.second->GetPayload().size() << std::endl;
             
             // Print the payload
             for (auto const byte: ret_val_2.second->GetPayload()) {
-                std::cout << "diag_client_conversion2 byte: " << std::hex << static_cast<int>(byte) << std::endl;
+                std::cout << "diag_client_conversation2 byte: " << std::hex << static_cast<int>(byte) << std::endl;
             }
         }
 
-        // Disconnect Tester One from ECU1 with remote ip address 192.168.0.1
-        diag_client_conversion1.DisconnectFromDiagServer();
+        // Disconnect Tester One from ECU1 with remote ip address 192.168.0.2
+        diag_client_conversation1.DisconnectFromDiagServer();
 
-        // Disconnect Tester Two from ECU2 with remote ip address 192.168.0.2
-        diag_client_conversion2.DisconnectFromDiagServer();
+        // Disconnect Tester Two from ECU2 with remote ip address 192.168.0.3
+        diag_client_conversation2.DisconnectFromDiagServer();
 
         std::this_thread::sleep_for(std::chrono::seconds(2));
     }    
     
-    // shutdown all the conversion
-    diag_client_conversion1.Shutdown();
-    diag_client_conversion2.Shutdown();
+    // shutdown all the conversation
+    diag_client_conversation1.Shutdown();
+    diag_client_conversation2.Shutdown();
 
     // important to release all the resources by calling de-inilialize
     diagclient->DeInitialize();
