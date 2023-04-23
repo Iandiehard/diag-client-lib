017a448b5cc08ddab5a34f74f89b068f7d207d07
Avijit Dey <subhoavijit@gmail.com>
2023-04-23 20:43:39 +0200

    Pipeline jobs set-up

--- .devcontainer/post_start.sh
+++ .devcontainer/post_start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # do network set-up
-sudo ip link add eth1 type dummy
+ip link add eth1 type dummy
 ifconfig eth1:0 172.16.25.127
 ifconfig eth1:1 172.16.25.128
-sudo ip link set dev eth1 up
+ip link set dev eth1 up
 
 printf "Network Setup Completed \n"
