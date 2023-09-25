9c4df4817b8f80058d25851cd93f0c1218dd4866
Avijit Dey <subhoavijit@gmail.com>
2023-09-25 13:17:08 +0200

    Set up pipeline for different compiler

--- tools/run_clang_format.sh
+++ tools/run_clang_format.sh
@@ -3,10 +3,10 @@
 DIAG_LIB_SRC_PATH="./diag-client-lib"
 DIAG_LIB_EXAMPLE_PATH="./examples"
 DIAG_LIB_TEST_PATH="./test"
-CLANG_FORMAT="clang-format-14"
+CLANG_FORMAT_VERSION="14"
 
 DIAG_LIB_SRC_LIST="$(find "$DIAG_LIB_SRC_PATH" | grep -E ".*(\.ino|\.cpp|\.c|\.h|\.hpp|\.hh)$")"
 DIAG_LIB_EXAMPLE_LIST="$(find "$DIAG_LIB_EXAMPLE_PATH" | grep -E ".*(\.ino|\.cpp|\.c|\.h|\.hpp|\.hh)$")"
 DIAG_LIB_TEST_LIST="$(find "$DIAG_LIB_TEST_PATH" | grep -E ".*(\.ino|\.cpp|\.c|\.h|\.hpp|\.hh)$")"
 
-$CLANG_FORMAT --verbose -i --style=file $DIAG_LIB_SRC_LIST $DIAG_LIB_EXAMPLE_LIST $DIAG_LIB_TEST_LIST
+clang-format-$CLANG_FORMAT_VERSION--verbose -i --style=file "$DIAG_LIB_SRC_LIST" "$DIAG_LIB_EXAMPLE_LIST" "$DIAG_LIB_TEST_LIST"
