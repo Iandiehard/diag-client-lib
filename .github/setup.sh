3535620c4b571eb9165e209503a6bba5a6495080
Avijit Dey <subhoavijit@gmail.com>
2023-09-25 13:46:28 +0200

    Set up pipeline for different compiler

--- .github/setup.sh
+++ .github/setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 apt-get update -qq
 
-# install compilers
-apt-get install clang-9
+# Install compilers
+apt-get install clang-14
+apt-get install g++-9
 
-# install Boost
+# Install Boost library
 BOOST_MAJOR_VERSION="1"
 BOOST_MINOR_VERSION="79"
 
 wget "https://boostorg.jfrog.io/artifactory/main/release/${BOOST_MAJOR_VERSION}.${BOOST_MINOR_VERSION}.0/source/boost_${BOOST_MAJOR_VERSION}_${BOOST_MINOR_VERSION}_0.tar.gz"
-
 mkdir boost
 tar -zxvf boost_${BOOST_MAJOR_VERSION}_${BOOST_MINOR_VERSION}_0.tar.gz -C boost
 cd boost/boost_${BOOST_MAJOR_VERSION}_${BOOST_MINOR_VERSION}_0 || exit
@@ -17,12 +17,12 @@ cd boost/boost_${BOOST_MAJOR_VERSION}_${BOOST_MINOR_VERSION}_0 || exit
 ./b2 variant=release install
 cd ../../
 
-# install DLT daemon
+# Install DLT daemon (needed for logging)
 DLT_MAJOR_VERSION="2"
 DLT_MINOR_VERSION="18"
 DLT_PATCH_VERSION="8"
 wget "https://github.com/COVESA/dlt-daemon/archive/refs/tags/v${DLT_MAJOR_VERSION}.${DLT_MINOR_VERSION}.${DLT_PATCH_VERSION}.tar.gz"
-mkdir dlt-daemon 
+mkdir dlt-daemon
 tar -xvf v${DLT_MAJOR_VERSION}.${DLT_MINOR_VERSION}.${DLT_PATCH_VERSION}.tar.gz -C dlt-daemon
 mkdir -p dlt-daemon/dlt-daemon-${DLT_MAJOR_VERSION}.${DLT_MINOR_VERSION}.${DLT_PATCH_VERSION}/build
 cd dlt-daemon/dlt-daemon-${DLT_MAJOR_VERSION}.${DLT_MINOR_VERSION}.${DLT_PATCH_VERSION} || exit
--- toolchains/linux_clang_14.cmake
+++ toolchains/linux_clang_14.cmake
@@ -1,5 +1,5 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-set(CMAKE_CXX_COMPILER              clang++-9)
+set(CMAKE_CXX_COMPILER              /usr/bin/clang++-14)
 set(CMAKE_CXX_FLAGS                 "-Wall -Werror -Wextra -Wpedantic -Wconversion -Wcast-align -Wunused -Wshadow -Wold-style-cast -Wpointer-arith -Wcast-qual -Wno-missing-braces" CACHE INTERNAL "")
