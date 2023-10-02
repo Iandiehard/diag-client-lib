e93dc95f983323ff709733e0ed13f365c58f6743
Avijit Dey <subhoavijit@gmail.com>
2023-10-02 13:16:44 +0200

    Split workflows based on compiler

--- build_with_gcc_9.sh
+++ build_with_gcc_9.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 BUILD_TYPE="Debug"
-BUILD_DIR="gcc9"
+COMPILER_NAME="gcc9"
+BUILD_DIR=$COMPILER_NAME-$BUILD_TYPE
 
 if [ ! -d build/$BUILD_DIR ]; then
   echo "***************** Create build folder for clang 14 *************************"
   mkdir -p build/$BUILD_DIR
 fi
-
 
 echo "********************** Configure CMake *************************************"
 cmake -B build/$BUILD_DIR -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DBUILD_WITH_DLT:BOOL=ON -DBUILD_WITH_TEST:BOOL=ON -DBUILD_DOXYGEN:BOOL=ON -DBUILD_SHARED_LIBS:BOOL=OFF -DBUILD_EXAMPLES:BOOL=ON -DCMAKE_INSTALL_PREFIX:PATH=build/install -DCMAKE_TOOLCHAIN_FILE=toolchains/linux_gcc_9.cmake
